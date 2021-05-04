425e6f16f38e2456f631c6c66fc2585d1641aea6
Natalie Enkin <natalie@enkin.co.il>
2021-05-04 10:46:34 +0300

    adding unmounting lifecycle method

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import "./App.css";
 import Card from "./components/MyCard/MyCard";
+import { Button } from "react-bootstrap";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <p>Hello react</p>
-      <Card index={1} />
-      <Card index={2} />
-      <Card index={3} />
-      <Card index={4} />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMounted_1: true,
+      isMounted_2: true,
+      isMounted_3: true,
+      isMounted_4: true,
+    };
+  }
+
+  removeFromDom = (index) => {
+    this.setState({ [`isMounted_${index}`]: false }, () =>
+      console.log(` removed card number ${index}`)
+    );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p>Hello react</p>
+        <div>
+          {this.state.isMounted_1 && <Card index={1} />}
+          <Button onClick={(e) => this.removeFromDom(1)}>delete me</Button>
+        </div>
+
+        <div>
+          {this.state.isMounted_2 && <Card index={2} />}
+          <Button onClick={(e) => this.removeFromDom(2)}>delete me</Button>
+        </div>
+        <div>
+          {this.state.isMounted_3 && <Card index={3} />}
+          <Button onClick={(e) => this.removeFromDom(3)}>delete me</Button>
+        </div>
+        <Card index={4} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/MyCard/MyCard.jsx
+++ src/components/MyCard/MyCard.jsx
@@ -7,13 +7,22 @@ export default class MyCard extends Component {
     super(props);
     this.state = {
       title: null,
+      number: 0,
     };
   }
-  updateTitle = () => {
-    this.setState({ title: `my new title is ${this.props.index}` }, () =>
-      console.log("title is updated")
+
+  updateTitle = (e) => {
+    // console.log(e);
+    let updatedNumber = ++this.state.number;
+    this.setState(
+      {
+        title: `my new title is ${this.props.index}`,
+        number: updatedNumber,
+      },
+      () => console.log("title is updated")
     );
   };
+
   componentDidUpdate(prevProps, prevState) {
     console.log("after component update");
     console.log(prevProps, prevState);
@@ -22,26 +31,29 @@ export default class MyCard extends Component {
   componentDidMount() {
     console.log(`card of index ${this.props.index} is mounted in the dom `);
   }
+  componentWillUnmount() {
+    console.log(
+      `card component is unmounted having number ${this.state.number}`
+    );
+  }
   render() {
     return (
-      <div>
-        <Card style={{ width: "18rem", border: "1px solid black" }}>
-          <Card.Body>
-            <Card.Title>
-              {this.state.title
-                ? this.state.title
-                : `Card number ${this.props.index}`}
-            </Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary" onClick={this.updateTitle}>
-              update card title
-            </Button>
-          </Card.Body>
-        </Card>
-      </div>
+      <Card style={{ width: "18rem", border: "1px solid black" }}>
+        <Card.Body>
+          <Card.Title>
+            {this.state.title
+              ? this.state.title
+              : `Card number ${this.props.index}`}
+          </Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary" onClick={this.updateTitle}>
+            update card title
+          </Button>
+        </Card.Body>
+      </Card>
     );
   }
 }
